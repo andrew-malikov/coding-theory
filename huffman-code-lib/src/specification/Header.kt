d7ecc338cd6f35ba9ea7fc6cbe373c7b1454ac5c
andrew <andrew.devels@gmail.com>
2018-11-02 20:59:26 +0500

    huffman-code-lib: Add a specification for encoding and decoding binary files

---
+++ huffman-code-lib/src/specification/Header.kt
@@ -0,0 +1,9 @@
+package huffman.code.lib.specification
+
+class Header {
+    var symbolLength: Byte = 8
+    var frequencyLength: Byte = 16
+    var symbolCount: Short = 0
+    var emptyBitCount: Byte = 0
+    var statsTable: String = ""
+}
---
+++ huffman-code-lib/src/specification/Payload.kt
@@ -0,0 +1,3 @@
+package huffman.code.lib.specification
+
+class Payload(val data: String)
